92d159a5c0118e543373a660107efd1d6b5a855f
Sam Jolley <samjolley@users.noreply.github.com>
2023-02-23 10:38:24 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -18,11 +18,9 @@
 *               |NUM   |      |      |                              |      |      |FUN   |
 *               `--------------------'                              `--------------------'
 *
-*   Encoder Left: Up <-> Down
-*   Encoder Right: Left <-> Right
 */
 
 &kp J,             &kp G,             &kp M,             &kp P,             &kp V,             &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,            \
 U_MT(LCTRL, R),    U_MT(LALT, S),     U_MT(LGUI, N),     U_MT(LSHFT, D),    &kp B,             &kp AMPS,          U_MT(LSHFT, A),    U_MT(LGUI, E),    U_MT(LALT, I),      U_MT(LCTRL, H),      \
 U_LT(U_BUTTON, X), U_MT(RALT, F),     &kp L,             &kp C,             &kp W,             &kp MINUS,         &kp U,             &kp O,            U_MT(RALT, Y),      U_LT(U_BUTTON, K),   \
-                                      U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT(U_SYM, T),    U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, ESC),
+                                      U_LT(U_NUM, DEL),  U_LT(U_SYM, BSPC), U_LT(U_NAV, T),   U_LT(U_SYM, TAB),   U_LT(U_NAV, SPACE),U_LT(U_FUN, DEL,
