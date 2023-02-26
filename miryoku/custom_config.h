fe01f69d5f5f7ed6490b3db0495aacd1eb719cf8
Sam Jolley <samjolley@users.noreply.github.com>
2023-02-26 18:05:42 -0500

    Update custom_config.h
    Correct combo errors

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -31,23 +31,23 @@
                 COMBO(combo_CUT,       K_CUT,           20 22,         0 1 2)  // cut
                 COMBO(combo_COPY,      K_COPY,          21 22,         0 1 2)  // copy
                 COMBO(combo_PASTE,     K_PASTE,         22 23,         0 1 2)  // paste
-                COMBO(combo_PSTM,      LS(PASTE),       21 23,         0 1 2)  // paste match
+                COMBO(combo_PSTM,      LS(K_PASTE),     21 23,         0 1 2)  // paste match
                 COMBO(combo_SALL,      LC(S),           20 23,         0 1 2)  // select all
                 COMBO(combo_KILL,      LA(F4),          20  4,         0 1 2)  // force quit
-                COMBO(combo_SCLP,      LS(LGUI(S)),     0  29,         0 1 2)  // screenshot
+                COMBO(combo_SCLP,      LS(LG(S)),       0  29,         0 1 2)  // screenshot
                 COMBO(combo_CAPS,      CAPS,            23 26,         0 1 2)  // CAPS LOCK
                 COMBO(combo_FIND,      K_FIND,          21 24,         0 1 2)  // find 
-                COMBO(combo_DELETE,    TAB,             13 31,         0 1 2)  // tab 
-                COMBO(combo_DELETE,    TAB,             16 31,         0 1 2)  // tab 
+                COMBO(combo_DELETE_L,  TAB,             13 31,         0 1 2)  // tab 
+                COMBO(combo_DELETE_R,  TAB,             16 31,         0 1 2)  // tab 
 
                 };
     };
 
 #define MIRYOKU_LAYER_BASE \
-&kp J,             &kp G,             &kp M,              &kp P,              &kp V,           &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,           \
-U_MT(LGUI, R),     U_MT(LALT, S),     U_MT(LCTRL, N),     U_MT(LSHFT, D),     &kp B,           &kp AMPS,          U_MT(LSHFT, A),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, H),      \
-U_LT(U_BUTTON, X), U_MT(RALT, F),     &kp L,              &kp C,              &kp W,           &kp MINUS,         &kp U,             &kp O,             U_MT(RALT, Y),     U_LT(U_BUTTON, K),  \
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, T),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+&kp J,             &kp G,             &kp M,             &kp P,             &kp V,             &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,           \
+U_MT(LGUI, R),     U_MT(LALT, S),     U_MT(LCTRL, N),    U_MT(LSHFT, D),    &kp B,             &kp AMPS,          U_MT(LSHFT, A),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, H),      \
+U_LT(U_BUTTON, X), U_MT(RALT, F),     &kp L,             &kp C,             &kp W,             &kp MINUS,         &kp U,             &kp O,             U_MT(RALT, Y),     U_LT(U_BUTTON, K),  \
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, T),  U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_TAP \
 &kp J,             &kp G,             &kp M,             &kp P,             &kp V,             &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,           \
