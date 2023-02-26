683e7290787cd5dcf7f33b0cb74278ab75bb5178
Sam Jolley <samjolley@users.noreply.github.com>
2023-02-26 15:38:32 -0500

    Update custom_config.h
    Fixed keycode spelling errors, especially for combos

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -31,9 +31,9 @@
                 COMBO(combo_CUT,       K_CUT,           20 22,         0 1 2)  // cut
                 COMBO(combo_COPY,      K_COPY,          21 22,         0 1 2)  // copy
                 COMBO(combo_PASTE,     K_PASTE,         22 23,         0 1 2)  // paste
-                COMBO(combo_PSTM,      LSHFT(PASTE),    21 23,         0 1 2)  // paste match
-                COMBO(combo_SALL,      LCTL(S),         20 23,         0 1 2)  // select all
-                COMBO(combo_KILL,      LALT(F4),        20  4,         0 1 2)  // force quit
+                COMBO(combo_PSTM,      LS(PASTE),       21 23,         0 1 2)  // paste match
+                COMBO(combo_SALL,      LC(S),           20 23,         0 1 2)  // select all
+                COMBO(combo_KILL,      LA(F4),          20  4,         0 1 2)  // force quit
                 COMBO(combo_SCLP,      LS(LGUI(S)),     0  29,         0 1 2)  // screenshot
                 COMBO(combo_CAPS,      CAPS,            23 26,         0 1 2)  // CAPS LOCK
                 COMBO(combo_FIND,      K_FIND,          21 24,         0 1 2)  // find 
