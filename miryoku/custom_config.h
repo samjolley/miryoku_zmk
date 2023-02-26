4512f0ca3eeb23fa6af6ed107335e69e75269d74
Sam Jolley <samjolley@users.noreply.github.com>
2023-02-26 15:25:19 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -19,7 +19,6 @@
     layers = <LAYERS>;                        \
   };
 
-/ {
         combos {
                 compatible = "zmk,combos";
 
@@ -40,17 +39,13 @@
                 COMBO(combo_DELETE,    TAB,             13 31,         0 1 2)  // tab 
                 COMBO(combo_DELETE,    TAB,             16 31,         0 1 2)  // tab 
 
-        };
-  };
-
-
-
+                };
 
 #define MIRYOKU_LAYER_BASE \
-&kp J,             &kp G,             &kp M,              &kp P,              &kp V,                &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,           \
-U_MT(LGUI, R),     U_MT(LALT, S),     U_MT(LCTRL, N),     U_MT(LSHFT, D),     &kp B,                &kp AMPS,          U_MT(LSHFT, A),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, H),      \
-U_LT(U_BUTTON, X), U_MT(RALT, F),     &kp L,              &kp C,              &kp W,                &kp MINUS,         &kp U,             &kp O,             U_MT(RALT, Y),     U_LT(U_BUTTON, K),  \
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, T),     U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+&kp J,             &kp G,             &kp M,              &kp P,              &kp V,           &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,           \
+U_MT(LGUI, R),     U_MT(LALT, S),     U_MT(LCTRL, N),     U_MT(LSHFT, D),     &kp B,           &kp AMPS,          U_MT(LSHFT, A),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, H),      \
+U_LT(U_BUTTON, X), U_MT(RALT, F),     &kp L,              &kp C,              &kp W,           &kp MINUS,         &kp U,             &kp O,             U_MT(RALT, Y),     U_LT(U_BUTTON, K),  \
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, T),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_TAP \
 &kp J,             &kp G,             &kp M,             &kp P,             &kp V,             &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,           \
