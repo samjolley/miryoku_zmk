4432d506757a65b314a7ff09e95197734ce00c5d
Sam Jolley <samjolley@users.noreply.github.com>
2023-02-27 11:21:37 -0500

    Add combos.dtsi

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,16 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-/*                KEY POSITIONS
-
-  ╭────────────────────╮ ╭────────────────────╮
-  │  0   1   2   3   4 │ │  5   6   7   8   9 │
-  │ 10  11  12  13  14 │ │ 15  16  17  18  19 │
-  │ 20  21  22  23  24 | | 25  26  27  28  29 │
-  ╰───────╮ 30  31  23 │ │ 33  34  35 ╭───────╯
-          ╰────────────╯ ╰────────────╯            
-*/
-
+#include <combos.dtsi>
 
 #define MIRYOKU_LAYER_BASE \
 &kp J,             &kp G,             &kp M,             &kp P,             &kp V,             &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,           \
