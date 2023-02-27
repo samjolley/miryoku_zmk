644df2c96e7b4181f29cd45d9cb6105b90dfcd3c
Sam Jolley <samjolley@users.noreply.github.com>
2023-02-27 11:29:20 -0500

    Move combos.dtsi

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#include <combos.dtsi>
-
 #define MIRYOKU_LAYER_BASE \
 &kp J,             &kp G,             &kp M,             &kp P,             &kp V,             &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,           \
 U_MT(LGUI, R),     U_MT(LALT, S),     U_MT(LCTRL, N),    U_MT(LSHFT, D),    &kp B,             &kp AMPS,          U_MT(LSHFT, A),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, H),      \
