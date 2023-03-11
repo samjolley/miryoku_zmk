029e5ee0ae10e61c9b124175fc7394cbd734e92c
Sam Jolley <samjolley@users.noreply.github.com>
2023-03-11 16:07:49 -0500

    Troublshoot nice!wiew

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,11 +1,12 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-CONFIG_BT_CTLR_TX_PWR_PLUS_8=y
-
 // Add Hands Down Gold (Neu-TX) as alpha layer
 #define MIRYOKU_LAYER_BASE \
 &kp J,             &kp G,             &kp M,             &kp P,             &kp V,             &kp SEMI,          &kp COMMA,         &kp DOT,           &kp SLASH,         &kp BSLH,           \
-U_MT(LCTRL, R),    U_MT(LALT, S),     U_MT(LGUI, N),     U_MT(LSHFT, D),    &kp B,             &kp AMPS,          U_MT(LSHFT, A),    U_MT(LGUI, E),     U_MT(LALT, I),     U_MT(LCTRL, H),      \
+U_MT(LCTRL, R),    U_MT(LALT, S),     U_MT(LGUI, N),     U_MT(LSHFT, D),    &kp B,             &kp AMPS,          U_MT(LSHFT, A),    U_MT(LGUI, E),     U_MT(LALT, I),     U_MT(LCTRL, H),     \
 U_LT(U_BUTTON, X), U_MT(RALT, F),     &kp L,             &kp C,             &kp W,             &kp MINUS,         &kp U,             &kp O,             U_MT(RALT, Y),     U_LT(U_BUTTON, K),  \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, DEL),  U_LT(U_MOUSE, T),  U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE),U_LT(U_FUN, BSPC), U_NP,              U_NP
+
+CONFIG_BT_CTLR_TX_PWR_PLUS_8=y
+CONFIG_ZMK_DISPLAY=y k
